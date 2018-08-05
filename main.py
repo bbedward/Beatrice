7a507e6bb77c14f39d7acb7cfa2f327361d67230
bbedward <bbedward@gmail.com>
2018-08-05 12:29:37 -0400

    Tab err

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ async def price(ctx):
         embed.add_field(name='BANANO-NANO', value='{0:.2f} BAN : 1 NANO'.format(banpernan))
         embed.add_field(name='BANANO 24H Vol.', value='{0:.6f} NANO'.format(volume))
         embed.add_field(name='BANANO USD', value='${0:.4f} : 1 BAN'.format(usdprice))
-	btc_usd = await api.get_btc_usd()
+    btc_usd = await api.get_btc_usd()
     mcap = 3402832669 * usdprice
     rank = await api.get_banano_rank(mcap, 1000)
     if btc_usd is not None:
