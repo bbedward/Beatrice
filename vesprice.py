670406654073b361760a7d5a289e7048a78ebd16
Brandon Berhent <bbedward@gmail.com>
2024-06-26 19:35:35 -0400

    Fix vesprice cron

--- vesprice.py
+++ vesprice.py
@@ -6,14 +6,25 @@ import json
 rdata = redis.StrictRedis(host=os.getenv(
     'REDIS_HOST', 'localhost'), port=6379)
 
-dolartoday_price = 'https://s3.amazonaws.com/dolartoday/data.json'
+dolartoday_price = "https://dolartoday.com/wp-admin/admin-ajax.php"
+
+# Form data to be sent with the request
+data = {
+    "action": "dt_currency_calculator_handler",
+    "amount": 1
+}
+
 
 def dolartoday_bolivar():
-    response = json.loads(requests.get(url=dolartoday_price).text)
-    if "USD" not in response:
+    response = json.loads(requests.post(url=dolartoday_price, data=data).text)
+    if "Dólar Bitcoin" not in response:
         print("Invalid response " + str(response))
         return
-    bolivarprice = response['USD']['localbitcoin_ref']
+    # Get the "Dólar Bitcoin" value
+    bitcoin_dollar_value = response["Dólar Bitcoin"]
+
+    # Extract the numerical value from the string
+    bolivarprice = float(bitcoin_dollar_value.split()[1])
     if bolivarprice is None:
         print("Couldn't find localbitcoin_ref price")
         return
